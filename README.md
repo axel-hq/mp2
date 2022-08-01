03217eaf07c3f9b0d067bcee1ba0ea48936d762f
AS-Pando <106735646+AS-Pando@users.noreply.github.com>
2022-08-01 11:36:59 -0700

    garbage code time

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# MP2
+# mp2
+
+*Server deploy orchestrator*
+
+So when you upload a new server, two things have to be configured.
+You have to tell nginx to properly route to the server and you need to tell pm2
+to restart it if it shuts down.
+
+The big issue is that you can only listen on port 433 (https) once in all of
+your nginx config files. That means we need to have one nginx config file that
+routes to each of the servers.
+
+mp2 is highly dependent on the directory structure because I don't care enough
+to make it configurable.
+
+Make the server directory structure like so:
+
+```
+/
+> pub/
+>  >  mp2/
+>  >  server-v*/
+>  >  >  package.json
+```
+
+Individual server versions should invoke mp2 in their deploy scripts.
+
+```
+node /pub/mp2 eyJzZXJ2ZXJfbmFtZSI6InNlcnZlci12MSIsInJlbGF0aXZlX21haW4iOiJiaW4vbWFpbi5qcyIsInNlcnZlcl9wb3J0Ijo0MDEzfQ==
+```
--- index.js
+++ index.js
@@ -1,18 +1,96 @@
 const fs = require("fs");
 const pm2 = require("pm2");
 
-function _(hof) {
-   return {then($){ hof((..._) => $(_)) }};
+process.chdir(__dirname);
+
+try {
+   const managed_raw = fs.readFileSync("managed.json");
+   var managed = JSON.parse(managed_raw);
+} catch (e) {
+   console.log(`Couldn't read ${__dirname}/managed.json`);
+   var managed = {};
 }
 
-void async function ()
-{
-void 0;
-const server_config = 
-var [err] = await _(pm2.connect.bind(pm2));
-if (err != null) throw err;
-var [err, processes] = await _(pm2.list.bind(pm2));
-if (err != null) throw err;
-console.log(processes);
-pm2.disconnect();
-}()
+function get_help(...something_else) {
+   throw new Error([...something_else
+      ,  "mp2 add <base64 encoded json config>"
+      ,  ""
+      ,  "type config = {"
+      ,  "   SERVER_NAME: string;"
+      ,  "   RELATIVE_MAIN: string;"
+      ,  "   SERVER_PORT: number;"
+      ,  "};"
+      ,  ""
+      ,  "const example_config = {"
+      ,  "   SERVER_NAME: 'server-v1',"
+      ,  "   RELATIVE_MAIN: 'bin/main.js'"
+      ,  "   SERVER_PORT: 4013,",
+      ,  "};"
+      ,  ""
+      ,  "mp2 subtract SERVER_NAME"
+      ,  ""
+      ,  "mp2 list"
+   ].join("\n"))
+}
+
+if (process.argv.length < 4) {
+   get_help("I expected you to provide two arguments to mp2!");
+}
+
+function then_ish(hof) {
+   return (...args) => ({then($){ hof(...args, (..._) => $(_)) }});
+}
+
+const pm3 = new Proxy(pm2, {
+   get(target, key) {
+      if (typeof target[key] === "function") {
+         return then_ish(target[key].bind(target));
+      } else {
+         return target[key];
+      }
+   }
+});
+
+switch (process.argv[2]) {
+case "subtract": subtract();
+case "sync": sync();
+case "add": add();
+default: get_help(`I don't know what ${process.argv[2]} means!`);
+}
+
+async function subtract() {
+   const name = process.argv[3];
+   var [err] = await pm3.connect();
+   if (err != null) throw err;
+   var [err, processes] = await pm3.delete(name);
+   await pm3.disconnect();
+   if (err != null) throw err;
+   process.exit(0);
+}
+
+async function add(name) {
+   const new_config = JSON.parse(atob(process.argv[2]));
+
+   if ("SERVER_NAME" in config) {} else {
+      get_help("Missing SERVER_NAME field!");
+   }
+   if ("RELATIVE_MAIN" in config) {} else {
+      get_help("Missing RELATIVE_MAIN field!");
+   }
+   if ("SERVER_PORT" in config) {} else {
+      get_help("missing SERVER_PORT field!");
+   }
+
+   manages[new_config.SERVER_NAME] = {
+      RELATIVE_NAME: 
+   }
+}
+
+// void async function () {
+//    var [err] = await pm3.connect;
+//    if (err != null) throw err;
+//    var [err, processes] = await pm3.list;
+//    if (err != null) throw err;
+//    console.log(processes);
+//    await pm3.disconnect;
+// }()
